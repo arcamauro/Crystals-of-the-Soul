805ddb11ef13561d5ab0de56ccf694e17e073546
Claudio Russo <claudiorusso02@gmail.com>
2026-05-13 18:57:33 +0200

    user story movimento

---
+++ core/src/main/java/io/github/crystals_of_the_soul/input/InputHandler.java
@@ -0,0 +1,24 @@
+package io.github.crystals_of_the_soul.input;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+public class InputHandler {
+
+    public float getDx() {
+        float dx = 0;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)) dx--;
+        if (Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)) dx++;
+
+        return dx;
+    }
+
+    public float getDy() {
+        float dy = 0;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.UP)) dy++;
+        if (Gdx.input.isKeyPressed(Input.Keys.S) || Gdx.input.isKeyPressed(Input.Keys.DOWN)) dy--;
+
+        return dy;
+    }
+}
---
+++ core/src/main/java/io/github/crystals_of_the_soul/player/Player.java
@@ -0,0 +1,19 @@
+package io.github.crystals_of_the_soul.player;
+
+public class Player {
+    private float x, y;
+    private float speed = 200f;
+
+    public Player(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update(float dx, float dy, float delta) {
+        x += dx * speed * delta;
+        y += dy * speed * delta;
+    }
+
+    public float getX() { return x; }
+    public float getY() { return y; }
+}
