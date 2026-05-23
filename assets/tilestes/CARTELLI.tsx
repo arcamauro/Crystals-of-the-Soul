4b94c3b8b2b55546de9b47c61143544279f45350
Gianni <gianniromano2005@gmail.com>
2026-05-23 21:24:02 +0200

    aggiunta collisioni
    #15

--- assets/tilestes/CARTELLI.tsx
+++ assets/tilestes/CARTELLI.tsx
@@ -6,6 +6,16 @@
    <object id="1" x="1" y="1" width="14" height="15"/>
   </objectgroup>
  </tile>
+ <tile id="266">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="12" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="267">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="12" height="16"/>
+  </objectgroup>
+ </tile>
  <tile id="273">
   <objectgroup draworder="index" id="2">
    <object id="1" x="1" y="0" width="14" height="15"/>
@@ -16,9 +26,44 @@
    <object id="1" x="0" y="1" width="16" height="15"/>
   </objectgroup>
  </tile>
+ <tile id="285">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="2" width="13" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="286">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="2" width="13" height="12"/>
+  </objectgroup>
+ </tile>
  <tile id="293">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="1" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="323">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="0" width="10" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="324">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="11" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="361">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="13" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="362">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="11" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="380">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5" y="11" width="8" height="5"/>
   </objectgroup>
  </tile>
 </tileset>
